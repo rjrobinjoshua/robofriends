54c33ffeb7d5f549213365e49412ce39ed84d6fa
robinjoshua <rjrobinjoshua@gmail.com>
2020-04-30 22:45:23 +0530

    completed inital version that fetches robots and displays them

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import 'tachyons'
 const Card = ({name, email, id}) => {
 	return (
 		<div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-2">
-			<img alt="robots" src={`https://robohash.org/${id}?size=200x200`}/>
+			<img alt="robots" src={`https://robohash.org/${name}?size=200x200`}/>
 			<div>
 				<h2>{name}</h2>
 				<p>{email}</p>
