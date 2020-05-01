f6b7fa77c05999eccbf410e18b1575fb25b20cac
robinjoshua <rjrobinjoshua@gmail.com>
2020-05-01 15:21:25 +0530

    Added scroll component and refacotred the structure

---
+++ src/customRobots.js
@@ -0,0 +1,14 @@
+export const customRobots = [
+    {
+        id: 100,
+        name: 'Robin Joshua',
+        username: 'robin',
+        email: 'robinjoshua@gmail.com'
+    },
+    {
+        id: 101,
+        name: 'EvanjalinJoshua',
+        username: 'evan',
+        email: 'Evanjalinjoshua@gmail.com'
+    }
+] 
